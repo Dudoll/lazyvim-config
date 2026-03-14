5c142c5d8ca43b0ece40f57eee44f28fd0fa4e7f
Joel <35829084+Dudoll@users.noreply.github.com>
2026-03-14 13:22:25 +0800

    Configure tab settings and relative number in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
 vim.o.mouse = ""
+vim.o.relativenumber = false
+vim.o.tabstop = 8
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.expandtab = false
